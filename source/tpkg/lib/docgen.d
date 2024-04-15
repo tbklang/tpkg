dbc6c3bdf4aaa0ab100c3515500ac89f9be37903
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-04-15 14:26:32 +0200

    Doc Gen
    - Now generates module pages

--- source/tpkg/lib/docgen.d
+++ source/tpkg/lib/docgen.d
@@ -8,15 +8,18 @@ import std.string : format;
 public class DocumentGenerator
 {
     private Program program;
+    private string directory;
+
+    // Current file being written to
     private File fileOut;
 
     // Writer state
     private bool listActive = false;
 
-    this(File fileOut, Program program)
+    // TOD: Take in a directory rather
+    this(string directory, Program program)
     {
-        // TODO: Throw on unopened file
-        this.fileOut = fileOut;
+        this.directory = directory;
         this.program = program;
     }
 
@@ -27,14 +30,76 @@ public class DocumentGenerator
 
     public void generate()
     {
+        // Generate the front page (module listing)
+        generateFrontPage(this.program);
+
+        // Generate each module's page
+        foreach(Module mod; this.program.getModules())
+        {
+            generateModulePage(mod);
+        }
+    }
+
+    public void generateModulePage(Module mod)
+    {
+        openFile(format("%s.html", mod.getName()));
+
+        scope(exit)
+        {
+            closeFile();
+        }
+
+        title(format("Module %s", mod.getName()));
+
+    }
+
+    private void title(string text)
+    {
+        // TODO: This should set <title> as well but maybe
+        // in some seperate flush-later few buffer(s),
+        // hence line() should be replaced with
+        // a buffer that is flushed later
+        line(format("<h1>%s</h1>", text));
+    }
+
+    private void openFile(string filename)
+    {
+        // TODO: bail if already open (means you didn't close)
+        if(this.fileOut.isOpen())
+        {
+            throw new Exception("Cannot call openFile(string) if there is already a file open. This is a developer bug");
+        }
+
+        this.fileOut.open(this.directory~"/"~filename, "wb");
+    }
+
+    private void closeFile()
+    {
+        if(!this.fileOut.isOpen())
+        {
+            throw new Exception("Cannot call closeFile() if there is NO file open. This is a developer bug");
+        }
+
+        this.fileOut.flush();
+        this.fileOut.close();
+    }
+
+    public void generateFrontPage(Program program)
+    {
+        openFile("index.html");
+
+        scope(exit)
+        {
+            closeFile();
+        }
+
         line("<html>");
         line("<body>");
         line(format("<h1>Program</h1>"));
         line("<h4>Program documentation</h4>");
         hr(fileOut);
 
-        generateModuleListing(this.program);
-
+        generateModuleListing(program);
     }
 
     private void hr(File fileOut)
@@ -107,7 +172,7 @@ unittest
     c.doParse();
 
     Program prog = c.getProgram();
-    DocumentGenerator dg = new DocumentGenerator(testF, prog);
+    DocumentGenerator dg = new DocumentGenerator("out/", prog);
 
     dg.generate();
 }
