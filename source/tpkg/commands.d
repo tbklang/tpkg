b048987fd839a3197302d173c8803198174d5e25
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-04-15 11:22:50 +0200

    Init command
    - Added initial command

--- source/tpkg/commands.d
+++ source/tpkg/commands.d
@@ -5,6 +5,9 @@ import tpkg.logging;
 
 import std.stdio : stdin;
 import niknaks.mechanisms : Prompter, Prompt;
+import tpkg.lib.project : Project;
+import std.json;
+import std.string : format;
 
 private mixin template BaseCommands()
 {
@@ -26,5 +29,19 @@ struct InitCommand
 
 
         // TODO: Prompt project information here
+        Prompter prompter = new Prompter(stdin);
+        prompter.addPrompt(Prompt("Project name: "));
+        prompter.addPrompt(Prompt("Project description: "));
+
+
+        Prompt[] answers = prompter.prompt();
+
+        Project proj;
+        proj.setName(answers[0].getValue());
+        proj.setDescription(answers[1].getValue());
+
+        JSONValue json = proj.serialize();
+        string jsonStr = json.toPrettyString();
+        DEBUG(format("Generated project descriptor:\n%s", jsonStr));
     }
 }
