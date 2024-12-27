0ca698d98152d6316d6cad29fdf36fde8a2257c3
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-12-27 12:55:05 +0200

    Manager (unittests)
    - Use new tetsing code

--- source/tpkg/lib/manager.d
+++ source/tpkg/lib/manager.d
@@ -763,26 +763,7 @@ version(unittest)
 
 version(unittest)
 {
-    alias DV = DummyVersion;
-    class DummyVersion : Version
-    {
-        private string ver_s;
-        this(string ver)
-        {
-            this.ver_s = ver;
-        }
-
-        public long cmp(Version rhs)
-        {
-            return ver_s == rhs.repr() ? 0 : -1; // FIXME: Implement me
-            // return 0; 
-        }
-
-        public string repr()
-        {
-            return ver_s;
-        }
-    }
+    import tpkg.lib.testing : DV;
 }
 
 unittest
